7a43b64ba2d32f062a47bd2a3ae5c7e23f387773
Starwort <tcphone93@gmail.com>
2020-02-27 21:50:30 +0000

    Allow ignoring `self` param

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "Code": "https://github.com/Starwort/LibSPLIWACA",
         "Issue tracker": "https://github.com/Starwort/LibSPLIWACA/issues",
     },
-    version="0.0.4",
+    version="0.0.5",
     packages=["libsplw"],
     include_package_data=True,
     # install_requires=REQUIREMENTS,
