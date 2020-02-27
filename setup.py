5fd8d1247e2292fd20fa58e80994377db6e04cfb
Starwort <tcphone93@gmail.com>
2020-02-27 21:25:01 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "Code": "https://github.com/Starwort/LibSPLIWACA",
         "Issue tracker": "https://github.com/Starwort/LibSPLIWACA/issues",
     },
-    version="0.0.1",
+    version="0.0.2",
     packages=["libsplw"],
     include_package_data=True,
     install_requires=REQUIREMENTS,
